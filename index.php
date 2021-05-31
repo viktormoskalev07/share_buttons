e8ac76174f476eae6e34c0805fa2acb74ef4403d
unknown <viktormoskalev07@gmail.com>
2021-05-31 15:52:51 +0300

    start

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+
+<html dir="ltr" lang="ru">
+<head>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>share</title>
+<meta name="description" content="share" /> 
+<script src="share.js"></script>
+<link href="style.css" rel="stylesheet">
+</head>
+<body>
+<div align="center" class="fond">
+
+<div class="logo" style="background: #bdf8ff;padding-top: 10px;padding-bottom: 10px;">
+   <a itemprop="name" href="https://web-stydia.com" title="Создание, разработка, изготовление сайтов | WEB-STYDIA.COM" target="_blank">						
+       <img class="logo-default" src="logo-w.png" width="227" height="60" alt="logo">
+   </a>
+</div>
+
+<div id="share">
+	<div class="like">share</div>
+<div class="social" id='social-info' data-url="<?php echo 'https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];?>" data-desc="description" data-title=" title">		
+	
+		<a class="push facebook" data-id="fb">  Facebook</a>  
+		<a class="push twitter" data-id="tw"> Twitter</a> 
+		<a class="push pinterest" data-id="pin">  Pinterest </a> 
+		<a class="push twitter" data-id="lin">  lin </a> 
+	</div>
+</div>
+https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fweb-stydia.com%2Fwp-content%2Fuploads%2Fexample_file%2Fshare-sicial-a%2F
+https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fshare-sicial-a%2F
+</div>
+</body>
+</html>
